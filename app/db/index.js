c200a05089ccdf100933a9bcf8c616c5dd9fe63a
koko <koko@koko-vm.myguest.virtualbox.org>
2024-04-18 16:23:59 +0800

    fix k8s yml

--- app/db/index.js
+++ app/db/index.js
@@ -8,7 +8,7 @@ class Basketball {
   constructor() {
     this.client = new Client({
       user: 'postgres',
-      host: 'postgres-fantasyBK',//postgres-fantasyBK//postgres-service
+      host: 'postgres-service',//postgres-fantasyBK//postgres-service
       database: 'basketball',
       password: 'postgres',
       port: 5432,
