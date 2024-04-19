ebba2466921fa72ef44901e81ff3d9cef178541d
koko <koko@koko-vm.myguest.virtualbox.org>
2024-04-19 17:57:43 +0800

    feat player table

--- app/db/index.js
+++ app/db/index.js
@@ -8,7 +8,7 @@ class Basketball {
   constructor() {
     this.client = new Client({
       user: 'postgres',
-      host: 'postgres-fantasyBK',//docker:postgres-fantasyBK//k8s:postgres-service
+      host: '127.0.0.1',//docker:postgres-fantasyBK//k8s:postgres-service
       database: 'basketball',
       password: 'postgres',
       port: 5432,
